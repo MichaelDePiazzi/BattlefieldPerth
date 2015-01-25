0ce1026f70dffd6e011e0661cbe81d0c4beb0c37
Michael De Piazzi <michael.de.piazzi@gmail.com>
2015-01-25 15:56:48 +0800

    Server: Added ScenarioO

--- Server/WhatWillWeDoNowServer/GameState/ScenarioTemplates/ScenarioO.cs
+++ Server/WhatWillWeDoNowServer/GameState/ScenarioTemplates/ScenarioO.cs
@@ -7,15 +7,15 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
         public ScenarioO()
         {
             Id = "O";
-            Title = "Vic Park";
+            Title = "Maylands Golf Course";
             ImageIndex = (int)GameState.ImageIndex.ScenarioO;
-            Text = "Walking through the streets of East Perth, you can hear a loud humming from somewhere unseen. As you approach the noise, the smell of ozone and burning metal fills the air. Rounding the corner, a massive triangular sculpture rises from a roundabout, filled with lightning, arcing off whenever anything gets close.";
+            Text = "Walking down the café strip, the aliens seem to have hunted you down. The Aliens are closing in your position, and preparing to attack. The Aliens split into small squads, and begin closing in on the Balmoral, as you hide in the old pub.";
             Choices = new[]
                 {
-                    "Try to disable the device",
-                    "Try to sneak past",
-                    "Turn around and leave",
-                    "Hide Behind another Person"
+                    "Fight Back",
+                    "Head further East",
+                    "Head to Tonkin Highway",
+                    "Drink at the Bar"
                 };
             Outcomes = new[]
                 {
@@ -29,16 +29,15 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
         private static Outcome CreateOutcome1()
         {
             return new Outcome
+            {
+                IsActive = players =>
+                    (players.Count(p => p.SelectedChoice == Choice.A) >= players.Count(p => p.SelectedChoice == Choice.B) + players.Count(p => p.SelectedChoice == Choice.C)),
+                ActionOutcomeAndGetDisplayText = players =>
                 {
-                    IsActive = players =>
-                        (players.Count(p => p.SelectedChoice == Choice.A) > (players.Count(p => p.SelectedChoice == Choice.B) + (players.Count(p => p.SelectedChoice == Choice.C)))),
-                    ActionOutcomeAndGetDisplayText = players =>
-                    {
-                        GameStateManager.DamagePlayers(players, player => (player.SelectedChoice == Choice.B));
-                        return "You start to throw debris and other objects towards the sparking tower, and as they fly close to the triangle, giant flashes of energy streak out and vaporise the projectiles in the air. One of you gets the bright idea of driving a car into the base of the tower. Starting at the top of the hill, you start the wreck rolling down the hill, and manage to steer it towards the base of the tower. The driver bails out of the car and lands safely at the edge of the road. The car smashes into the base of the tower, as lightning strikes at it continuously. The impact causes the whole structure to topple and fall to the side, inactive. Anyone standing to close gets a shock from the residual energy burning them badly. The team walks north, and heads towards Mount Lawley.";
-                    },
-                    NextScenarioKey = "Q"
-                };
+                    return "You look through the Bar rooms, and all you can find to defend yourself are pool cues. Using the pool chalk from the bar, you paint your faces with blue war paint. You shout out loud as you rush them all with your improvised weapons, shouting something about lives and freedom. You are killed quickly afterwards, as your bodies are riddled with flechettes.";
+                },
+                NextScenarioKey = "X"
+            };
         }
 
         private static Outcome CreateOutcome2()
@@ -46,20 +45,12 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
             return new Outcome
             {
                 IsActive = players =>
-                    (players.Count(p => p.SelectedChoice == Choice.B) > (players.Count(p => p.SelectedChoice == Choice.A) + (players.Count(p => p.SelectedChoice == Choice.C)))),
+                    (players.Count(p => p.SelectedChoice == Choice.B) >= players.Count(p => p.SelectedChoice == Choice.A) + players.Count(p => p.SelectedChoice == Choice.C)),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    var damagedPlayers = GameStateManager.DamagePlayers(players,
-                        player => player.SelectedChoice == Choice.A || player.SelectedChoice == Choice.C);
-
-                    string extraText = "";
-                    // if players were damaged, add more text
-                    if (damagedPlayers.Count > 0)
-                        extraText = GameStateManager.GetPlayerNames(damagedPlayers) + " stray too close and get a shock from the tower, causing painful burns.* After making it past, You head towards Mount Lawley";
-
-                    return "Staying away from the energy tower seems like the best course of action, however your destination is past it. While experimenting with the range of the device, you think you can sneak around the device by staying out of range. Walking between buildings and around the alleyways, you make it most of the way around. The last hurdle is sliding along a wall, keeping as far as you can from the energy. " + extraText;
+                    return "You continue East, fleeing from the invasion, You eventually come to the freight line, just as a diesel train is taking off from the station. Racing after it you climb aboard and wave to the driver. Breathing out, you think, Kalgoorlie might a bit safer, the Aliens seem to like the humidity.";
                 },
-                NextScenarioKey = "Q"
+                NextScenarioKey = "V"
             };
         }
 
@@ -68,20 +59,12 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
             return new Outcome
             {
                 IsActive = players =>
-                    (players.Count(p => p.SelectedChoice == Choice.C) > (players.Count(p => p.SelectedChoice == Choice.A) + (players.Count(p => p.SelectedChoice == Choice.B)))),
+                    (players.Count(p => p.SelectedChoice == Choice.C) >= players.Count(p => p.SelectedChoice == Choice.A) + players.Count(p => p.SelectedChoice == Choice.B)),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    var damagedPlayers = GameStateManager.DamagePlayers(players,
-                        player => player.SelectedChoice == Choice.A || player.SelectedChoice == Choice.B);
-
-                     string extraText = "";
-                    // if players were damaged, add more text
-                     if (damagedPlayers.Count > 0)
-                         extraText = GameStateManager.GetPlayerNames(damagedPlayers) + "stray too close during their investigations, and get shocked by the tower. ";
-
-                     return "The lightning tower is an intimidating sight, and you decide it is insurmountable. " + extraText + "Everyone turns and walks back the up the hill and towards the WACA";
+                    return "Grabbing a ute from the back of the pub, you all pile into the back, and head up Tonkin towards the Swan Valley. You think you can find enough food to eat, places to hide and enough weapons to fight back.";
                 },
-                NextScenarioKey = "R"
+                NextScenarioKey = "U"
             };
         }
 
@@ -95,11 +78,10 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
                     (players.Count(p => p.SelectedChoice == Choice.C) == 0),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    return "Everyone nervously looks around, hanging back, trying to stay further back than any other person. " + players[0].Name + " throws their hands in the air and exclaims, \"Screw this shit, let's go another way\". Nods of agreement start from the rest of the group, as you all turn and head towards the WACA.";
+                    return "Sitting down and resigning to your fate, you pour out some drinks for anyone left alive. You all knock back the scotch, and stat a game of pool, waiting for the invaders to come and find you.  A loud hissing noise fills the area, scanning the skies a swarm of flying insects fills the sky from the South, they swarm into the building hunting down anything they can. They fly into you tearing through your bodies, as you fall to the ground, they swarm all over you, and feast on your flesh.";
                 },
-                NextScenarioKey = "R"
+                NextScenarioKey = "X"
             };
         }
-
     }
 }
