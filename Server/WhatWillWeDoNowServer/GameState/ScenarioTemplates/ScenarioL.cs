7a183710187805b325ed754bdfb4686ef35b9127
Michael De Piazzi <michael.de.piazzi@gmail.com>
2015-01-25 14:05:52 +0800

    Server: Added scenario L

--- Server/WhatWillWeDoNowServer/GameState/ScenarioTemplates/ScenarioL.cs
+++ Server/WhatWillWeDoNowServer/GameState/ScenarioTemplates/ScenarioL.cs
@@ -9,13 +9,13 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
             Id = "L";
             Title = "Burswood";
             ImageIndex = (int)GameState.ImageIndex.ScenarioL;
-            Text = "Walking through the streets of East Perth, you can hear a loud humming from somewhere unseen. As you approach the noise, the smell of ozone and burning metal fills the air. Rounding the corner, a massive triangular sculpture rises from a roundabout, filled with lightning, arcing off whenever anything gets close.";
+            Text = "The trapezoidal building smokes from fires inside, and a pulsing tentacle bursts from the side of the building, and snakes towards the river. Walking through the car park you find a side entry into the building, and duck inside. There seems to be alien activity inside, as the whole atmosphere inside the building is wet, and leaves a stinging taste in your mouth. Smaller aliens scuttle through the halls, ignoring your presence.";
             Choices = new[]
                 {
-                    "Try to disable the device",
-                    "Try to sneak past",
-                    "Turn around and leave",
-                    "Hide Behind another Person"
+                    "Follow the small creatures",
+                    "Head for the River",
+                    "Go back outside",
+                    "Find a stocked Bar"
                 };
             Outcomes = new[]
                 {
@@ -29,16 +29,15 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
         private static Outcome CreateOutcome1()
         {
             return new Outcome
+            {
+                IsActive = players =>
+                    (players.Count(p => p.SelectedChoice == Choice.A) >= players.Count(p => p.SelectedChoice == Choice.B) + players.Count(p => p.SelectedChoice == Choice.C)),
+                ActionOutcomeAndGetDisplayText = players =>
                 {
-                    IsActive = players =>
-                        (players.Count(p => p.SelectedChoice == Choice.A) > (players.Count(p => p.SelectedChoice == Choice.B) + (players.Count(p => p.SelectedChoice == Choice.C)))),
-                    ActionOutcomeAndGetDisplayText = players =>
-                    {
-                        GameStateManager.DamagePlayers(players, player => (player.SelectedChoice == Choice.B));
-                        return "You start to throw debris and other objects towards the sparking tower, and as they fly close to the triangle, giant flashes of energy streak out and vaporise the projectiles in the air. One of you gets the bright idea of driving a car into the base of the tower. Starting at the top of the hill, you start the wreck rolling down the hill, and manage to steer it towards the base of the tower. The driver bails out of the car and lands safely at the edge of the road. The car smashes into the base of the tower, as lightning strikes at it continuously. The impact causes the whole structure to topple and fall to the side, inactive. Anyone standing to close gets a shock from the residual energy burning them badly. The team walks north, and heads towards Mount Lawley.";
-                    },
-                    NextScenarioKey = "N"
-                };
+                    return "Following the small creatures, you head down into the car park and the basement below. The air gets thicker and thicker as you progress. You round a corner, and find a large basement full of spires of gelatinous goo. You inspect a tower and see it has been built around the body of a person, swimming through the gel are numerous small creatures feeding off the flesh of the body. A loud splattering noise startles you from behind, as a spire bursts apart, and hundreds of larger creatures swarm towards you. More and more spires burst apart, and you are devoured as their first taste of warm flesh.";
+                },
+                NextScenarioKey = "X"
+            };
         }
 
         private static Outcome CreateOutcome2()
@@ -46,18 +45,10 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
             return new Outcome
             {
                 IsActive = players =>
-                    (players.Count(p => p.SelectedChoice == Choice.B) > (players.Count(p => p.SelectedChoice == Choice.A) + (players.Count(p => p.SelectedChoice == Choice.C)))),
+                    (players.Count(p => p.SelectedChoice == Choice.B) >= players.Count(p => p.SelectedChoice == Choice.A) + players.Count(p => p.SelectedChoice == Choice.C)),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    var damagedPlayers = GameStateManager.DamagePlayers(players,
-                        player => player.SelectedChoice == Choice.A || player.SelectedChoice == Choice.C);
-
-                    string extraText = "";
-                    // if players were damaged, add more text
-                    if (damagedPlayers.Count > 0)
-                        extraText = GameStateManager.GetPlayerNames(damagedPlayers) + " stray too close and get a shock from the tower, causing painful burns.* After making it past, You head towards Mount Lawley";
-
-                    return "Staying away from the energy tower seems like the best course of action, however your destination is past it. While experimenting with the range of the device, you think you can sneak around the device by staying out of range. Walking between buildings and around the alleyways, you make it most of the way around. The last hurdle is sliding along a wall, keeping as far as you can from the energy. " + extraText;
+                    return "Heading towards the river you can see the large tentacle is pumping water into the casino for some nefarious purpose.  A speed boat is here and you all pile in. You take off up the Swan river, however after a few minutes of cruising, the engine starts to splutter, there is no fuel in the tanks. You drift to shore and head towards the Maylands golf course.";
                 },
                 NextScenarioKey = "N"
             };
@@ -68,18 +59,10 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
             return new Outcome
             {
                 IsActive = players =>
-                    (players.Count(p => p.SelectedChoice == Choice.C) > (players.Count(p => p.SelectedChoice == Choice.A) + (players.Count(p => p.SelectedChoice == Choice.B)))),
+                    (players.Count(p => p.SelectedChoice == Choice.C) >= players.Count(p => p.SelectedChoice == Choice.A) + players.Count(p => p.SelectedChoice == Choice.B)),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    var damagedPlayers = GameStateManager.DamagePlayers(players,
-                        player => player.SelectedChoice == Choice.A || player.SelectedChoice == Choice.B);
-
-                     string extraText = "";
-                    // if players were damaged, add more text
-                     if (damagedPlayers.Count > 0)
-                         extraText = GameStateManager.GetPlayerNames(damagedPlayers) + "stray too close during their investigations, and get shocked by the tower. ";
-
-                     return "The lightning tower is an intimidating sight, and you decide it is insurmountable. " + extraText + "Everyone turns and walks back the up the hill and towards the WACA";
+                    return "Something about this feels wrong, and you all agree, time to turn around and head the other way. As you all walk into Vic Park, a painfully loud screech comes from the casino, as swarms of bugs fly from the building, and head to the west.";
                 },
                 NextScenarioKey = "O"
             };
@@ -95,11 +78,10 @@ namespace WhatWillWeDoNowServer.GameState.ScenarioTemplates
                     (players.Count(p => p.SelectedChoice == Choice.C) == 0),
                 ActionOutcomeAndGetDisplayText = players =>
                 {
-                    return "Everyone nervously looks around, hanging back, trying to stay further back than any other person. " + players[0].Name + " throws their hands in the air and exclaims, \"Screw this shit, let's go another way\". Nods of agreement start from the rest of the group, as you all turn and head towards the WACA.";
+                    return "You turn away from the creature, and head for the high rollers room. It is empty, and unlocked. You all sit round a table, and pour out glasses of 30 year old scotch. You all sit back and prepare for the end. It comes swiftly as swarms of flying insects flood the room, and begin to feed on your flesh.";
                 },
-                NextScenarioKey = "O"
+                NextScenarioKey = "X"
             };
         }
-
     }
 }
